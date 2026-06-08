6f94901fce8d0706b0b6496fe8e8819f9c623290
Ajeng Puktri Karunia <ajeng.puktri.karunia.an24@stu.pnj.ac.id>
2026-06-08 08:05:55 +0700

    update halaman beasiswa

--- resources/views/beasiswa.blade.php
+++ resources/views/beasiswa.blade.php
@@ -1,17 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Beasiswa</title>
-</head>
-<body>
+@extends('layouts.app')
 
-<h1>Informasi Beasiswa</h1>
+@section('content')
+<div class="container mt-5">
+    <div class="p-5 mb-4 bg-light rounded-3 shadow">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold text-primary">Program Beasiswa IAST</h1>
+            <p class="col-md-8 fs-4">Halaman khusus informasi beasiswa bagi mahasiswa, dosen, dan peneliti di lingkungan IAST Institute.</p>
+        </div>
+    </div>
 
-<p>
-Daftar program beasiswa terbaru.
-</p>
-
-<a href="/">Kembali ke Home</a>
-
-</body>
-</html>
+    <div class="row align-items-md-stretch">
+        <div class="col-md-6">
+            <div class="h-100 p-5 text-bg-dark rounded-3">
+                <h2>Beasiswa Prestasi</h2>
+                <p>Untuk mahasiswa dengan IPK di atas 3.5.</p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+                <h2>Beasiswa Riset</h2>
+                <p>Dukungan dana bagi penelitian skripsi/tugas akhir.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
